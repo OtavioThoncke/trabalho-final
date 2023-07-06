73856828c52ad7b0b877b7381b21fff3a25f33c0
OtavioThoncke <130064849+OtavioThoncke@users.noreply.github.com>
2023-07-06 18:45:04 -0300

    Update CodigoBuilder.ts

--- CodigoBuilder.ts
+++ CodigoBuilder.ts
@@ -1,4 +1,5 @@
-// Classe para representar a casa
+// Codigo com builder :
+
 class House {
   foundation: string | null = null;
   walls: string | null = null;
@@ -91,3 +92,58 @@ director = new ConstructionDirector(luxuryBuilder);
 director.constructHouse();
 const luxuryHouse: House = director.getHouse();
 console.log(luxuryHouse.toString());
+
+
+// codigo sem builder:
+
+class House {
+  foundation: string;
+  walls: string;
+  roof: string;
+  floors: number;
+  rooms: number;
+  garage: boolean;
+  garden: boolean;
+
+  constructor(foundation: string, walls: string, roof: string, floors: number, rooms: number, garage: boolean, garden: boolean) {
+    this.foundation = foundation;
+    this.walls = walls;
+    this.roof = roof;
+    this.floors = floors;
+    this.rooms = rooms;
+    this.garage = garage;
+    this.garden = garden;
+  }
+
+  addGarage(): void {
+    this.garage = true;
+  }
+
+  addGarden(): void {
+    this.garden = true;
+  }
+
+  toString(): string {
+    let houseDescription = Casa com fundação: ${this.foundation}, paredes: ${this.walls}, telhado: ${this.roof}, ${this.floors} andares e ${this.rooms} quartos.;
+
+    if (this.garage) {
+      houseDescription += ' Possui garagem.';
+    }
+
+    if (this.garden) {
+      houseDescription += ' Possui jardim.';
+    }
+
+    return houseDescription;
+  }
+}
+
+// Construção de uma casa simples
+const simpleHouse = new House("Concreto", "Tijolos", "Telhas", 1, 3, true, false);
+simpleHouse.addGarden();
+console.log(simpleHouse.toString());
+
+// Construção de uma casa de luxo
+const luxuryHouse = new House("Mármore", "Vidro", "Telhado de vidro", 3, 6, true, true);
+console.log(luxuryHouse.toString());
+
